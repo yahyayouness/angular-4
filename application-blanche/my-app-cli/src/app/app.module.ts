690b9502f547ea737434e8792900d8baa9534b8d
yahya <yahyayounes@hotmail.fr>
2017-10-22 00:36:51 +0200

    generate first component with ng generate

---
+++ application-blanche/my-app-cli/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
